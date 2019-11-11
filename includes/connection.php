7a1f6bc691da5d3c037ac54cdf16e589753190ca
amaury2409 <amaury.henrottay@gmail.com>
2019-11-11 10:31:33 +0100

    validation and execute

--- includes/connection.php
+++ includes/connection.php
@@ -22,42 +22,33 @@ $github = filter_var($_POST['github'], FILTER_SANITIZE_URL);
 if ($email == "") {
     echo "please enter your email";
 } else {
-    echo filter_var($email, FILTER_VALIDATE_EMAIL) ? "email valid." : $errors['email'] = "This address is invalid." ;
+    filter_var($email, FILTER_VALIDATE_EMAIL) ? " email valid " : $errors['email'] = "This address is invalid." ;
 }
 
 if ($username == "") {
     echo "please enter your username";
 } elseif (!empty($username)) {
     $usernameLenght = strlen($username);
-    echo ($usernameLenght >255) ? $errors['username'] = "your username exceed 255 letters"  : 'valide' ;
+    ($usernameLenght <=255) ? ' username valide ' : $errors['username'] = "your username exceed 255 letters" ;
+}
+
+if ($passwordOne == "" || $passwordTwo == "") {
+    echo "please add or confirm your password";
+} elseif (!empty($passwordOne) && !empty($passwordTwo)) {
+    $password_crypted_One = sha1($passwordOne);
+    $password_crypted_Two = sha1($passwordTwo);
+    ($passwordOne === $passwordTwo) ? 'password valide' :  $errors['password'] = "your password are not similar"  ;
 }
 
 // execute
 
 if (count($errors)> 0) {
     echo "There are mistakes!";
+    echo "<pre>";
     print_r($errors);
+    echo "</pre>";
     exit;
 }
 
-
-
-// check errors
-// if (isset($_POST['inscription'])) {
-//     if (!empty($username) && !empty($passwordOne) && !empty($passwordOne) && !empty($email)) {
-//         $usernameLenght = strlen($username);
-//         if ($usernameLenght <= 255) {
-//             if ($passwordOne === $passwordTwo) {
-//                 $insertmbr = $pdo->prepare("INSERT INTO student(username,email,password) VALUE(?.?.?)");
+// $insertmbr = $pdo->prepare("INSERT INTO student(username,email,password) VALUE(?.?.?)");
 //                 $insertmbr->execute(array($username,$passwordOne,$email));
-//                 echo "compte bien crée";
-//             } else {
-//                 echo "les mots de passe ne sont pas similaire";
-//             }
-//         } else {
-//             $error = "votre pseudo ne doit pas dépasser 255 caractères ";
-//         }
-//     } else {
-//         $error = "Certains champs doit être complété";
-//     }
-// }
